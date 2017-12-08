6ce66203aa56cb9e087bfcaab5e3d6e08c22433c
michaelsnook <michael.snook@gmail.com>
2017-12-08 16:15:50 +0530

    Bang the initial home page format into shape

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,8 @@
-<footer class="content-info">
+<footer class="content-info bg-dark text-muted">
   <div class="container">
     @php(dynamic_sidebar('sidebar-footer'))
+  </div>
+  <div class="container">
+    This site is &copy; The People's Org 2017
   </div>
 </footer>
